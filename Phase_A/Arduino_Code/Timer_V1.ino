9e1e8cbe812edd2e4c703a3f3e1280a8c952f802
harshakarunanayaka <74353616+harshakarunanayaka@users.noreply.github.com>
2023-04-17 07:27:39 -0600

    Updated

---
+++ Phase_A/Arduino_Code/Timer_V1.ino
@@ -0,0 +1,128 @@
+#define BUTTON_1  2
+#define BUTTON_2  3
+#define GREEN_LED 4
+#define RED_LED   5
+//#define BUZZER    6
+
+#define DATA      9
+#define LATCH     8
+#define CLOCK     7
+
+#define DIGIT_4   10
+#define DIGIT_3   11
+#define DIGIT_2   12
+#define DIGIT_1   13
+ 
+unsigned char table[]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71,0x00};
+
+volatile int count = 30;
+
+volatile unsigned char button1_flag = 0;
+volatile unsigned char button2_flag = 0;
+
+void setup() {
+  // LEDs Pins
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+
+  // Button Pins
+  pinMode(BUTTON_1, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_1), Button_1_ISR, RISING);
+  pinMode(BUTTON_2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_2), Button_2_ISR, RISING);
+
+  // Buzer Pins
+  pinMode(BUZZER, OUTPUT);
+  digitalWrite(BUZZER,LOW);
+
+  // 7-Seg Display
+  pinMode(DIGIT_1, OUTPUT);
+  pinMode(DIGIT_2, OUTPUT);
+  pinMode(DIGIT_3, OUTPUT);
+  pinMode(DIGIT_4, OUTPUT);
+
+  // 7-Seg Display
+  pinMode(DIGIT_1, OUTPUT);
+  pinMode(DIGIT_2, OUTPUT);
+  pinMode(DIGIT_3, OUTPUT);
+  pinMode(DIGIT_4, OUTPUT);
+
+  // Shift Register Pins
+  pinMode(LATCH, OUTPUT);
+  pinMode(CLOCK, OUTPUT);
+  pinMode(DATA, OUTPUT);
+
+  disp_off();
+
+  // Initialize Timer2 (8bit) -> Used to refresh display
+  // Speed of Timer2 = 16MHz/1024 = 15.625 KHz
+  TCCR2A = 0;
+  TCCR2B = 0;
+  OCR2A = gReloadTimer2;                     // max value 2^8 - 1 = 255
+  TCCR2A |= (1<<WGM21);
+  TCCR2B = (1<<CS22) | (1<<CS21) | (1<<CS20); // 1204 prescaler
+  TIMSK2 |= (1<<OCIE2A);
+  interrupts();                               // enable all interrupts
+}
+
+void display(unsigned char num)
+{
+  for (int j =30 ;j<0;j--)
+  {
+    digitalWrite(LATCH, LOW);
+    shiftOut(DATA, CLOCK, LSBFIRST, table[num]);
+    digitalWrite(LATCH, HIGH);
+  }
+}
+
+void Display_Numbers()
+{
+  Display(count);
+
+ 
+}
+
+void disp_off()
+{
+   digitalWrite(DIGIT_1, HIGH);
+   digitalWrite(DIGIT_2, HIGH);
+   digitalWrite(DIGIT_3, HIGH);
+   digitalWrite(DIGIT_4, HIGH);
+}
+
+void Button_1_ISR()
+{
+   button1_flag=1;
+}
+
+void Button_2_ISR()
+{ 
+   count++;
+
+}
+
+void activeBuzzer()
+{
+  unsigned char i;
+  unsigned char sleepTime = 1; // ms
+  
+  for(i=0;i<100;i++)
+  {
+    digitalWrite(BUZZER,HIGH);
+    delay(sleepTime);//wait for 1ms
+    digitalWrite(BUZZER,LOW);
+    delay(sleepTime);//wait for 1ms
+  }
+}
+
+void loop() 
+{
+  if button1_flag==1
+  {
+    button1_flag = 0;
+    count--
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(GREEN_LED, HIGH);
+    
+  }
+}
